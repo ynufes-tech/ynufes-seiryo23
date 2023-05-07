2104d2031ce9efac9e4d8326f12ca95194522ab2
Shion Ichikawa <shion1305@gmail.com>
2023-05-08 03:17:16 +0900

    ✨ create HeaderPC

---
+++ components/HeaderPC.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+  <header>
+  </header>
+</template>
+
+<style scoped>
+header {
+    width: max(200px, 25vw);
+    height: 100vh;
+    background: #8dc0f5;
+}
+</style>
