f96f0cbae4efdeac880f08cb372fd39adbc25993
tomoyahiroe <tomoya4creative@gmail.com>
2023-05-08 04:41:25 +0900

    feat: primary button、スマホ未対応

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 <template>
   <DateCircle />
   <div class="access">
-    <SectionTitle section-title="アクセス" />
+    <SectionTitle class="access-title" section-title="アクセス" />
     <div class="train-access">
       <p>横浜市営地下鉄ブルーライン「三ツ沢上町駅」より正門まで徒歩16分</p>
       <p>相鉄線「和田町駅」より南門・南通用門まで徒歩20分</p>
@@ -14,16 +14,23 @@
 
 <style lang="scss" scoped>
 .access {
-  width: calc(90vw - 130px);
+  width: 100%;
   margin: 100px auto 0px;
+
+  .access-title {
+    margin: 0 auto;
+    width: fit-content;
+  }
 
   .train-access {
     border: 1px solid #3b3b3b;
     border-radius: 12px;
     padding: 20px;
     margin: 0 auto;
+    max-width: 95vw;
     width: fit-content;
     text-align: left;
+    box-sizing: border-box;
   }
 }
 </style>
