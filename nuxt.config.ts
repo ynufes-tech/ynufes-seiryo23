49ab4292203ed18158123a318657055a04535b54
Shion Ichikawa <shion1305@gmail.com>
2023-05-07 22:04:23 +0900

    ➕ Google Fontの導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  modules: ["@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "Konkhmer Sleokchher": [400, 500, 600, 700],
+    },
+  },
+});
