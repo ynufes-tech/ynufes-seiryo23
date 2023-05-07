0ef60e273551ff59bb5123332a6b4235dbf2dda3
Shion Ichikawa <shion1305@gmail.com>
2023-05-07 23:29:54 +0900

    🔧 フォントの追加・フォント設定書式修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/google-fonts"],
-  googleFonts: {
-    families: {
-      "Konkhmer Sleokchher": [400, 500, 600, 700],
-    },
-  },
+  modules: [
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          "Konkhmer Sleokchher": {
+            wght: [400, 500, 600, 700],
+          },
+          "Noto Sans JP": {
+            wght: [400, 500, 600, 700],
+          },
+        },
+      },
+    ],
+  ],
 });
