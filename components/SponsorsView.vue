799269293bb61499c52669529283c062161cf92d
Shion Ichikawa <shion1305@gmail.com>
2023-05-08 11:46:30 +0900

    ✨ SponsorsView

---
+++ components/SponsorsView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="sponsor-frame">
+    <a href="http://www.hazawa-kubotaganka.com/">
+      <img src="@/assets/images/JOINUS.jpg" />
+    </a>
+    <a href="https://www.sotetsu-joinus.com/">
+      <img src="@/assets/images/KUBOTA.png" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdsView",
+};
+</script>
+
+<style scoped lang="scss">
+.sponsor-frame {
+  display: flex;
+  margin: 30px auto;
+  flex-direction: row;
+  align-items: center;
+  max-width: 90%;
+  overflow: hidden;
+  box-sizing: border-box;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+
+  > a {
+    flex: 1;
+
+    > img {
+      width: 100%;
+      object-fit: contain;
+    }
+  }
+}
+</style>
