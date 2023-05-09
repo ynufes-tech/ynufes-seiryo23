864a5d4984290d7d94d10872212703864aba486b
Shion Ichikawa <shion1305@gmail.com>
2023-05-09 09:28:10 +0900

    🔍️ キーワードの追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    "~/assets/style/reset.css"
-  ],
+  css: ["~/assets/style/reset.css"],
   modules: [
     [
       "@nuxtjs/google-fonts",
       {
         families: {
           "Konkhmer Sleokchher": {
-            wght: [400, 500, 600, 700]
+            wght: [400, 500, 600, 700],
           },
           "Noto Sans JP": {
-            wght: [400, 500, 600, 700]
-          }
-        }
-      }
-    ]
+            wght: [400, 500, 600, 700],
+          },
+        },
+      },
+    ],
   ],
   app: {
     head: {
       htmlAttrs: {},
       meta: [
         { property: "og:type", content: "website" },
-        { property: "og:site_name", content: "清陵祭2023 - 横浜国立大学大学祭" },
+        {
+          property: "og:site_name",
+          content: "清陵祭2023 - 横浜国立大学大学祭",
+        },
         // { property: "og:image", content: "<ogpに使われる画像の絶対パス>" }
-        { property: "keywords", content: "清陵祭,横浜国立大学,大学祭,文化祭,清涼祭" }
-      ]
-    }
-
-  }
+        {
+          property: "keywords",
+          content:
+            "清陵祭,横浜国立大学,大学祭,文化祭,横国,清涼祭,せいりょうさい,2023",
+        },
+      ],
+    },
+  },
 });
