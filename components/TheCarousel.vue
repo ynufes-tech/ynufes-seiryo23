fabc7caba6ef5953fc7c7e3f90c7db75d887b459
tomoyahiroe <tomoya4creative@gmail.com>
2023-05-11 01:47:21 +0900

    add: カルーセルの追加

---
+++ components/TheCarousel.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Pagination, Autoplay } from 'swiper'
+</script>
+
+<template>
+  <swiper
+    :modules="[Pagination, Autoplay]"
+    :pagination="{ clickable: true }"
+    :autoplay="true"
+    slides-per-view="auto"
+    :loop="true"
+    :space-between="10"
+    :centered-slides="true"
+  >
+    <swiper-slide
+      v-for="image in useEventState().state.value.carouselImagePaths"
+      :key="image"
+      ><img :src="image" class="swiper-image"
+    /></swiper-slide>
+  </swiper>
+</template>
+
+<style lang="scss" scoped>
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  max-width: 900px;
+  height: 180px;
+
+  .swiper-image {
+    border-radius: 30px;
+    width: 100%;
+  }
+}
+</style>
