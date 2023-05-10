fa46565ce7b0d3ac96f929457a62b8102bd36a2b
tomoyahiroe <tomoya4creative@gmail.com>
2023-05-10 18:50:05 +0900

    add: 清陵祭についてページの作成

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <SectionTitle class="about-title" section-title="清陵祭とは" />
+</template>
+
+<style scoped>
+.about-title {
+  margin: 0 auto;
+  width: fit-content;
+}
+</style>
