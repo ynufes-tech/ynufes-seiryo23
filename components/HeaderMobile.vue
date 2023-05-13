d1d9890fe82b550f46032853aeb4d05956c665de
tomoyahiroe <tomoya4creative@gmail.com>
2023-05-13 16:56:36 +0900

    feat: モバイル時のメニュー作成

--- components/HeaderMobile.vue
+++ components/HeaderMobile.vue
@@ -1,4 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+let isOpen = ref(false);
+onMounted(() => {
+  const hamburger = document.getElementById("hamburger-menu");
+  if (hamburger) {
+    hamburger.addEventListener("click", () => {
+      isOpen.value = !isOpen.value;
+    });
+  }
+});
+const close = () => {
+  isOpen.value = false;
+};
+</script>
 
 <template>
   <div class="header-base">
@@ -7,7 +20,38 @@
         <h1>清陵祭2023</h1>
         <h2>横浜国立大学 大学祭</h2>
       </div>
+      <div id="hamburger-menu">
+        <span class="hamburger-bar"></span>
+        <span class="hamburger-bar"></span>
+        <span class="hamburger-bar"></span>
+      </div>
     </div>
+    <Transition name="hamburger">
+      <div v-if="isOpen">
+        <nav class="mobile-navigation" >
+          <ul>
+            <li>
+              <NuxtLink to="/" @click="close">ホーム</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about" @click="close"> 清陵祭について</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/events" @click="close">企画検索</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/schedule" @click="close">スケジュール</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/pamphlet" @click="close">パンフレット</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/sponsors" @click="close"> ご協賛について </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Transition>
   </div>
 </template>
 
@@ -15,8 +59,8 @@
 .header-base {
   font-family: "Noto Sans JP", sans-serif;
   font-weight: 700;
-  height: 90px;
   width: 90vw;
+  height: 90px;
   margin: 0 auto;
   opacity: 0.8;
   background: #93ebf6;
@@ -24,12 +68,12 @@
 }
 
 .header-base__title {
-  width: fit-content;
+  width: 90%;
   padding-left: 30px;
   height: 90px;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
 
   > div {
     width: fit-content;
@@ -51,5 +95,69 @@
       padding: 0;
     }
   }
+  #hamburger-menu {
+    width: 36px;
+    margin-right: 10px;
+
+    .hamburger-bar {
+      display: block;
+      width: 36px;
+      height: 2px;
+      background-color: #060707;
+      margin-bottom: 8px;
+      margin-left: auto;
+
+      &:first-child {
+        width: 28px;
+      }
+      &:nth-child(2) {
+        width: 16px;
+      }
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+}
+.mobile-navigation {
+  box-sizing: border-box;
+  background-color: #93ebf6;
+  position: relative;
+  top: -16px;
+  border-radius: 0 0 20px 20px;
+  display: flex;
+  justify-content: center;
+  ul {
+    margin: 0;
+    padding: 20px;
+  }
+  li {
+    list-style: none;
+    text-align: center;
+    margin-bottom: 12px;
+  }
+  a {
+    font-size: 20px;
+    font-weight: bold;
+    text-decoration: none;
+    color: #594e4edd;
+  }
+}
+.hamburger-enter-active,
+.hamburger-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.hamburger-enter-from,
+.hamburger-leave-to {
+  opacity: 0;
+}
+
+a.router-link-active {
+  color: #060707;
+  font-size: 2rem;
+  font-weight: bold;
+  text-decoration: none;
+  color: #594e4e;
 }
 </style>
