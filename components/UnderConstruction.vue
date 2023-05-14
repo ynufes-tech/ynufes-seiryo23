4c6fa0589648c9bba65f56919485c0fab72b7e93
tomoyahiroe <tomoya4creative@gmail.com>
2023-05-15 00:08:58 +0900

    add: 建設中ページを作成

---
+++ components/UnderConstruction.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Under Construction</h1>
+    <p>This page is under construction.</p>
+  </div>
+</template>
+
+<style scoped></style>
