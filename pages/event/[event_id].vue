0002895f8a71c2c8044f1190b0787dfa87bbff14
tomoyahiroe <tomoya4creative@gmail.com>
2023-05-17 13:05:06 +0900

    feat: データ(ダミー)の取得成功

--- pages/event/[event_id].vue
+++ pages/event/[event_id].vue
@@ -1,9 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import events from '@/assets/json/dammy.json';
+import { Genre, genreToString } from "../../model/genre";
+import { Place, placeToString } from "../../model/place";
+
+const route = useRoute();
+const event = events.find((event) => event.id === Number(route.params.event_id));
+</script>
 
 <template>
   <div>
-    {{ $route.params.event_id }}
+    <SectionFrame>
+      <p class="event-name">{{ event?.event_name }}</p>
+      <p class="org-name">{{ event?.org_name }}</p>
+    </SectionFrame>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.frame-section {
+  width: 90%;
+  text-align: center;
+  max-width: 600px;
+  position: relative;
+
+.event-name {
+  font-size: 20px;
+}
+
+  .org-name {
+    margin: 0;
+    padding: 8px 20px;
+    position: absolute;
+    bottom: -12px;
+    right: -12px;
+    background-color: rgb(167, 255, 255);
+    border-radius: 16px;
+  }
+}
+</style>
