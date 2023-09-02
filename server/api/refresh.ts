7656e1fc6c6c077bb6742054e2b84aa6127774d6
Shion Ichikawa <shion1305@gmail.com>
2023-09-02 16:24:08 +0900

    🔧 スライドの更新処理の停止の表記

--- server/api/refresh.ts
+++ server/api/refresh.ts
@@ -1,5 +1,10 @@
 import { updateSlides } from "~/server/slides";
 
+/**
+ * このエンドポイントはスライドの更新を行うためのものです。
+ * 現在スライドの更新がないため、このエンドポイントの呼び出しは無効化されています。
+ * 直接叩くことでスライドの更新を走らせることが可能です。
+ */
 export default defineEventHandler(async (event) => {
   await updateSlides();
   return { message: "ok" };
